b1ae23a26f4a7a6861fd0d60177e8c354549ac10
dilshadanikkadan <officialdilshad3312@gmail.com>
2024-04-04 11:16:17 +0530

    changes in index file new

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-
-import express from 'express';
+import express from "express";
 const app = express();
-const port = 3000; 
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-dotenv.config(); 
-import userRoute from './routes/userRoute.js';
-import adminRoute from './routes/adminRoute.js'
-import doctorRoute from './routes/doctorRoute.js'
-import chatRoute from './routes/chatRoute.js'
-import cookieParser from 'cookie-parser';
-import cors from 'cors'
-import http from "http" 
-import { Server } from 'socket.io';
-import NotificationContoller from './controller/chatController/notificationController.js';
+const port = 3000;
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+import userRoute from "./routes/userRoute.js";
+import adminRoute from "./routes/adminRoute.js";
+import doctorRoute from "./routes/doctorRoute.js";
+import chatRoute from "./routes/chatRoute.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
+import NotificationContoller from "./controller/chatController/notificationController.js";
 
 const corsOptions = {
-  origin: ['https://client-hospital-qv1u.vercel.app'],
+  origin: ["https://client-hospital-qv1u.vercel.app"],
   credentials: true,
-  methods:["GET","POST","PUT","DELETE"]
+  methods: ["GET", "POST", "PUT", "DELETE"],
 };
 //middlewires
 app.use(cors(corsOptions));
@@ -26,11 +25,10 @@ app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(cookieParser());
-app.use('/api/user', userRoute);
-app.use('/api/admin', adminRoute);
-app.use('/api/doctor', doctorRoute)
-app.use('/api/chat',chatRoute)
-
+app.use("/api/user", userRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/doctor", doctorRoute);
+app.use("/api/chat", chatRoute);
 
 // error middleware
 app.use((err, req, res, next) => {
@@ -44,32 +42,28 @@ app.use((err, req, res, next) => {
   });
 });
 
-// http set up for socket 
-const server = http.createServer(app)
+// http set up for socket
+const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "https://client-hospital-qv1u.vercel.app",
     methods: ["GET", "POST"],
-    credentials: true
-  }
+    credentials: true,
+    transports: ["websocket", "polling"],
+  },
+  allowEIO3: true,
 });
 
- 
-//appendig as argument  
-NotificationContoller(io)
-
-
-
-
+//appendig as argument
+NotificationContoller(io);
 
 /// Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {}, (err) => {
   if (err) throw err;
-  console.log('Connected to MongoDB');
-}) 
+  console.log("Connected to MongoDB");
+});
 
-//listening 
+//listening
 server.listen(3000, () => {
   console.log(`Server is running on port ${port}`);
-})
-
+});
